25e080551ff606e0b8879f2356f30dff51aa6f07
prkrishna2091 <prkrishna2091@gmail.com>
2023-08-06 20:27:33 +0530

    mbus-api-gateway

---
+++ src/main/java/com/rkgroup/mbusapigateway/MbusApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.rkgroup.mbusapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbusApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MbusApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rkgroup/mbusapigateway/MbusApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkgroup.mbusapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbusApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
